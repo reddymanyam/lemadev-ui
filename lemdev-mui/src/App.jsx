bd037164714bf7f913e2095d8f011a41db3fb328
reddymanyam <reddappareddy18@gmail.com>
2024-10-21 13:18:25 +0530

    updated

--- lemdev-mui/src/App.jsx
+++ lemdev-mui/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./App.css"
+import Sidebar from './components/Sidebar/Sidebar'
+import Rightbar from './components/Rightbar/Rightbar'
+import Feed from './components/Feed/Feed'
+import { Box, Stack } from '@mui/material'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <>
-       
-    </>
+    <Box >
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   )
 }
 
